6982b26c542920df3eabdfc5f8510dbfc551bce1
RippNasgorGoreng <rifarohman59@gmail.com>
2025-10-21 15:08:29 +0700

    Add files via upload

---
+++ Praktik_7.1_Fungsi_Rifa_XPPLG2/Praktik_7.1_Fungsi_Rifa_XPPLG2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik_7._1_Fungsi_Rifa_XPPLG2
+{
+    internal class Program
+    {
+        static void sapa()
+        {
+            Console.WriteLine("Halo selamat datang di pembelajaran C#!");
+            Console.WriteLine("Mari belajar tentang fungsi");
+        }
+     static void Main(string[] args)
+    {
+      sapa(); // Memanggil fungsi
+    }
+    }
+}
---
+++ Praktik_7.1_Fungsi_Rifa_XPPLG2/Praktik_7.1_Fungsi_Rifa_XPPLG2/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Praktik_7.1_Fungsi_Rifa_XPPLG2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Praktik_7.1_Fungsi_Rifa_XPPLG2")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d7a5a5ec-5fff-46ca-b2ef-3190e6300e14")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
