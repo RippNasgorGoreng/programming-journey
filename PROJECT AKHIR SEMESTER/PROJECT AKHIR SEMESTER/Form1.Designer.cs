b6905f2dfe041e6d03aac6560020925d0f742439
RippNasgorGoreng <rifarohman59@gmail.com>
2025-11-26 08:29:49 +0700

    Add files via upload

---
+++ PROJECT AKHIR SEMESTER/PROJECT AKHIR SEMESTER/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace PROJECT_AKHIR_SEMESTER
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            cmbpilper = new ComboBox();
+            button2 = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ActiveCaptionText;
+            label1.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(163, 21);
+            label1.Name = "label1";
+            label1.Size = new Size(628, 25);
+            label1.TabIndex = 0;
+            label1.Text = "PENJUALAN TIKET MASUK STADION MOH SARENGAT";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = SystemColors.ActiveCaptionText;
+            label2.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(277, 71);
+            label2.Name = "label2";
+            label2.Size = new Size(389, 23);
+            label2.TabIndex = 1;
+            label2.Text = "PILIH PERTANDINGAN YANG TERSEDIA";
+            label2.Click += label2_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.LightCyan;
+            button1.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(354, 258);
+            button1.Name = "button1";
+            button1.Size = new Size(217, 29);
+            button1.TabIndex = 2;
+            button1.Text = "CEK DAN PESAN TIKET";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // cmbpilper
+            // 
+            cmbpilper.BackColor = Color.White;
+            cmbpilper.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbpilper.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            cmbpilper.ForeColor = SystemColors.InfoText;
+            cmbpilper.FormattingEnabled = true;
+            cmbpilper.Location = new Point(325, 118);
+            cmbpilper.Name = "cmbpilper";
+            cmbpilper.Size = new Size(282, 31);
+            cmbpilper.TabIndex = 3;
+            cmbpilper.SelectedIndexChanged += cmbpilper_SelectedIndexChanged;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Red;
+            button2.Location = new Point(898, 559);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 4;
+            button2.Text = "Close";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox1.Location = new Point(833, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(131, 107);
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.stadion_psbt__1_;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1004, 600);
+            Controls.Add(pictureBox1);
+            Controls.Add(button2);
+            Controls.Add(cmbpilper);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private ComboBox cmbpilper;
+        private Button button2;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ PROJECT AKHIR SEMESTER/PROJECT AKHIR SEMESTER/Form4.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PROJECT_AKHIR_SEMESTER
+{
+    public partial class Form4 : Form
+    {
+        int tribun1 = 850;
+        int tribun2 = 850;
+        int tribunvip = 700;
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+            comboBox2.Items.Add("Tribun Timur");
+            comboBox2.Items.Add("Tribun Selatan");
+            comboBox2.Items.Add("Tribun VIP");
+
+
+            dataGridView1.Rows.Clear();
+            dataGridView1.Rows.Add("Tribun Timur", "Rp. 25.000", tribun1);
+            dataGridView1.Rows.Add("Tribun Selatan", "Rp. 25.000", tribun2);
+            dataGridView1.Rows.Add("Tribun VIP", "Rp. 50.000", tribunvip);
+
+        }
+
+        private void buttontampil_Click(object sender, EventArgs e)
+        {
+            if (comboBox2.Text == "Tribun Selatan")
+            {
+                string harga = $"{Convert.ToInt32(textBox1.Text) * 25000}";
+                dataGridView2.Rows.Add(label7.Text, "Tribun Selatan", textBox1.Text, harga);
+            }
+            else if (comboBox2.Text == "Tribun Timur")
+            {
+                string harga = $"{Convert.ToInt32(textBox1.Text) * 25000}";
+                dataGridView2.Rows.Add(label7.Text, "Tribun Timur", textBox1.Text, harga);
+            }
+            else if (comboBox2.Text == "Tribun VIP")
+            {
+                string harga = $"{Convert.ToInt32(textBox1.Text) * 50000}";
+                dataGridView2.Rows.Add(label7.Text, "Tribun VIP", textBox1.Text, harga);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            
+
+            if (string.IsNullOrWhiteSpace(textBox1.Text) ||
+                string.IsNullOrWhiteSpace(comboBox2.Text))
+                
+            {
+                MessageBox.Show("Silahkan Lengkapi Pemesanan Terlebih Dahulu", "peringatan", MessageBoxButtons.CancelTryContinue, MessageBoxIcon.Warning);
+                return;
+            }
+            else if (string.IsNullOrWhiteSpace(textBoxbayar.Text))
+            {
+                MessageBox.Show("Silahkan Bayar Pesanan Anda", "peringatan", MessageBoxButtons.CancelTryContinue, MessageBoxIcon.Warning);
+                return;
+            }
+            int stokktimbaru = 0;
+            int stokselbaru = 0;
+            int stokvipbaru = 0;
+            int harga = 0;
+            int uangbayar = Convert.ToInt32(textBoxbayar.Text);
+
+
+            if (comboBox2.Text == "Tribun Timur")
+            {
+                harga = Convert.ToInt32(textBox1.Text) * 25000;
+
+                tribun1 = tribun1 - Convert.ToInt32(textBox1.Text);
+                stokktimbaru = tribun1;
+                if (stokktimbaru >= 0 && uangbayar >= harga)
+                    if (dataGridView1.Rows.Count > 0)
+                    {
+                        // Jika AllowUserToAddRows = true, pastikan tidak hanya ada baris kosong
+                        if (!(dataGridView1.Rows.Count == 1 && dataGridView1.AllowUserToAddRows))
+                        {
+                            dataGridView1.Rows.RemoveAt(0); // Hapus baris pertama
+                            dataGridView1.Rows.Insert(0, "Tribun Timur", "Rp. 25.000", stokktimbaru);
+                        }
+                    }
+
+
+            }
+            if (comboBox2.Text == "Tribun Selatan")
+            {
+                harga = Convert.ToInt32(textBox1.Text) * 25000;
+
+                tribun2 = tribun2 - Convert.ToInt32(textBox1.Text);
+                stokselbaru = tribun2;
+                if (stokselbaru >= 0 && uangbayar >= harga)
+                    if (dataGridView1.Rows.Count > 0)
+                    {
+                        // Jika AllowUserToAddRows = true, pastikan tidak hanya ada baris kosong
+                        if (!(dataGridView1.Rows.Count == 2 && dataGridView1.AllowUserToAddRows))
+                        {
+                            dataGridView1.Rows.RemoveAt(1); // Hapus baris kedua
+                            dataGridView1.Rows.Insert(1, "Tribun Selatan", "Rp. 25.000", stokselbaru);
+                        }
+                    }
+
+
+
+
+            }
+            if (comboBox2.Text == "Tribun VIP")
+            {
+                harga = Convert.ToInt32(textBox1.Text) * 50000;
+                tribunvip = tribunvip - Convert.ToInt32(textBox1.Text);
+                stokvipbaru = tribunvip;
+                if (stokvipbaru >= 0 && uangbayar >= harga)
+                    if (dataGridView1.Rows.Count > 0)
+                    {
+                        // Jika AllowUserToAddRows = true, pastikan tidak hanya ada baris kosong
+                        if (!(dataGridView1.Rows.Count == 3 && dataGridView1.AllowUserToAddRows))
+                        {
+                            dataGridView1.Rows.RemoveAt(2); // Hapus baris ketiga
+                            dataGridView1.Rows.Insert(2, "Tribun VIP ", "Rp. 50.000", stokvipbaru);
+                        }
+                    }
+
+
+            }
+            else if (textBox1 == null)
+            {
+                MessageBox.Show("Harap isi jumlah beli sesuai stok tiket!");
+            }
+
+
+
+            if (!int.TryParse(textBoxbayar.Text, out uangbayar))
+            {
+                MessageBox.Show("Masukkan angka yang bener ya cukimay");
+                return;
+            }
+            if (uangbayar < harga)
+            {
+                MessageBox.Show("Uang Tidak Cukup");
+            }
+            else if (uangbayar == harga)
+            {
+                MessageBox.Show("Pembayaran Berhasil! Terima kasih.");
+            }
+            else
+            {
+                int kembalian = uangbayar - harga;
+                MessageBox.Show("Pembayaran Berhasil! \nKembalian Anda : Rp" + kembalian);
+            }
+        }
+
+        private void textBoxbayar_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBoxbayar.Text))
+            {
+                if (int.TryParse(textBoxbayar.Text, out int textboxbayar))
+                {
+                    return;
+                }
+                else
+                {
+                    textBoxbayar.Clear();
+                    MessageBox.Show("Isi Pakai Angka Yaaa, DASAR KANEBO KERING");
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView2.Rows.Clear();
+            comboBox2.SelectedIndex = -1;
+            textBox1.Clear();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+          "Apakah Anda yakin ingin menutup program?", "Konfirmasi", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                // Tutup semua form dan hentikan aplikasi
+                Application.Exit();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form1 Form1 = new Form1();
+            Form1.Show();
+            this.Close();
+        }
+
+        private void buttonhapusrows_Click(object sender, EventArgs e)
+        {
+
+            if (dataGridView2.Rows.Count > 0)
+            {
+                // Jika AllowUserToAddRows aktif, baris kosong di akhir harus diabaikan
+                int lastRowIndex = dataGridView2.AllowUserToAddRows
+                    ? dataGridView2.Rows.Count - 2   // Lewati baris kosong terakhir
+                    : dataGridView2.Rows.Count - 1;
+
+                if (lastRowIndex >= 0)
+                {
+                    dataGridView2.Rows.RemoveAt(lastRowIndex);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Tidak ada detail pesanan yang dapat dihapus!",
+                                "Peringatan",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+                if (int.TryParse(textBox1.Text, out int textbox1))
+                {
+                    if (comboBox2.Text == "Tribun Timur")
+                    {
+                        if (textbox1 > tribun1)
+                        {
+                            textBox1.Clear();
+                            MessageBox.Show("Pesanan melebihi Jumlah Stok!", "peringatan", MessageBoxButtons.CancelTryContinue, MessageBoxIcon.Warning);
+                        }
+                    }
+
+                    else if (comboBox2.Text == "Tribun Selatan")
+                    {
+                        if (textbox1 > tribun2)
+                        {
+                            textBox1.Clear();
+                            MessageBox.Show("Pesanan melebihi Jumlah Stok!", "peringatan", MessageBoxButtons.CancelTryContinue, MessageBoxIcon.Warning);
+                        }
+                    }
+
+                    else if (comboBox2.Text == "Tribun VIP")
+                    {
+                        if (textbox1 > tribunvip)
+                        {
+                            textBox1.Clear();
+                            MessageBox.Show("Pesanan melebihi Jumlah Stok!", "peringatan", MessageBoxButtons.CancelTryContinue, MessageBoxIcon.Warning);
+                        }
+                    }
+                }
+                else
+                {
+                    textBox1.Clear();
+                    MessageBox.Show("Isi Menggunakan Angka Yaa!");
+                }
+            }
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
